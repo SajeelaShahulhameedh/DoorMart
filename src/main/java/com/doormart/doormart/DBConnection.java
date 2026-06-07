79aeeefeca3d9a8254627958d53bab2059a32712
SajeelaShahulhameedh <sajeelashahulhameedh@gmail.com>
2026-06-07 15:02:34 +0530

    Update DBConnection.java
    DoorMart - Register, Login and Owner Dashboard

--- src/main/java/com/doormart/doormart/DBConnection.java
+++ src/main/java/com/doormart/doormart/DBConnection.java
@@ -16,7 +16,7 @@ public class DBConnection {
     
     private static final String URL = "jdbc:mysql://localhost:3306/doormart_db";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "your password here";
     
     public static Connection getConnection() throws SQLException {
         try {
